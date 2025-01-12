3f91a59f45769744ca5bfa6e450f1bf02ac808ae
Hubert Cywka <hcywka@gmail.com>
2025-01-12 11:33:56 +0100

    Use prototype pattern in all driver commands

--- src/main/java/edu/kis/powp/jobs2d/command/CompoundCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/CompoundCommand.java
@@ -4,6 +4,7 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CompoundCommand implements ICompoundCommand{
 
@@ -28,5 +29,11 @@ public class CompoundCommand implements ICompoundCommand{
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public CompoundCommand copy() {
+        List<DriverCommand> copiedCommandList = commandList.stream().map(DriverCommand::copy).collect(Collectors.toList());
+        return new CompoundCommand(copiedCommandList, name);
     }
 }
--- src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -20,4 +20,8 @@ public class SetPositionCommand implements DriverCommand {
         driver.setPosition(posX, posY);
     }
 
+    @Override
+    public SetPositionCommand copy() {
+        return new SetPositionCommand(posX, posY);
+    }
 }
