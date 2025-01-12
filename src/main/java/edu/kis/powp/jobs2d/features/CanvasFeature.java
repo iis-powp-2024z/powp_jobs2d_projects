abdf8af2193a7c601e0af25d33abcaabba291163
lukasz-kkk <lukaluzny17@gmail.com>
2025-01-12 11:09:37 +0100

    Added initial CanvasFeature class

---
+++ src/main/java/edu/kis/powp/jobs2d/features/CanvasFeature.java
@@ -0,0 +1,9 @@
+package edu.kis.powp.jobs2d.features;
+import edu.kis.powp.appbase.Application;
+
+public class CanvasFeature {
+    private static Application application;
+
+    public static void configCanvasFeature(Application app) {
+    }
+}
