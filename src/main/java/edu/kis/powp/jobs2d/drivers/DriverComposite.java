e3ae6759709a10a2cc07e6cc514f36fece7d0dad
BeastRacid <polskiarrow@protonmail.com>
2025-01-17 17:06:38 +0100

    Driver composite constructed

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
@@ -1,5 +1,26 @@
 package edu.kis.powp.jobs2d.drivers;
 
-public class DriverComposite  {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class DriverComposite {
+    private List<Job2dDriver> drivers = new ArrayList<>();
+
+    public void addDriver(Job2dDriver driver) {
+        drivers.add(driver);
+    }
+
+    public void operateTo(int x, int y) {
+        for (Job2dDriver driver : drivers) {
+            driver.operateTo(x, y);
+        }
+    }
+
+    public void setPosition(int x, int y) {
+        for (Job2dDriver driver : drivers) {
+            driver.setPosition(x, y);
+        }
+    }
 }
