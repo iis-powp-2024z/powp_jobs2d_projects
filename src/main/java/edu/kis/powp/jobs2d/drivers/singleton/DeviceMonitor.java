3cff858e3a07098b6a7e75e59a77357b5f1d4a30
Stanisław Horna <stanislawhorna@outlook.com>
2025-01-19 11:05:04 +0100

    add method calculateDistance

--- src/main/java/edu/kis/powp/jobs2d/drivers/singleton/DeviceMonitor.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/singleton/DeviceMonitor.java
@@ -1,19 +1,30 @@
 package edu.kis.powp.jobs2d.drivers.singleton;
 
 public final class DeviceMonitor {
-    public int previousX, previousY;
     private static DeviceMonitor instance;
+    private int previousX = 0, previousY = 0;
+    private double traversedDistance = 0.0;
 
     private DeviceMonitor(int previousX, int previousY) {
         this.previousX = previousX;
         this.previousY = previousY;
     }
 
-    public static DeviceMonitor getInstance(int previousX, int previousY) {
+    public static DeviceMonitor getInstance(int x, int y) {
         if (instance == null) {
-            instance = new DeviceMonitor(previousX, previousY);
+            instance = new DeviceMonitor(x, y);
         }
 
+        instance.calculateDistance(x, y);
         return instance;
     }
+
+    public void calculateDistance(int newX, int newY) {
+        //d=√((x_2-x_1)²+(y_2-y_1)²
+        double distance = Math.sqrt(Math.pow(newX - previousX, 2) + Math.pow(newY - previousY, 2));
+        traversedDistance += distance;
+        previousX = newX;
+        previousY = newY;
+    }
+
 }
