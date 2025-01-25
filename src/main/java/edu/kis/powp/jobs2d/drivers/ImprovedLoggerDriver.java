b895a87a76e51dcc916801a5314e404bb6be4039
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2025-01-25 18:22:09 +0100

    Add visit to imprved looger driver

--- src/main/java/edu/kis/powp/jobs2d/drivers/ImprovedLoggerDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/ImprovedLoggerDriver.java
@@ -1,6 +1,7 @@
 package edu.kis.powp.jobs2d.drivers;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.visitor.IVisitor;
 
 import java.util.logging.Logger;
 
@@ -31,5 +32,9 @@ public class ImprovedLoggerDriver implements Job2dDriver {
 
     public String toString() {
         return "Logger driver";
+    }
+
+    public void visit(IVisitor visitor) {
+        visitor.visit(this);
     }
 }
