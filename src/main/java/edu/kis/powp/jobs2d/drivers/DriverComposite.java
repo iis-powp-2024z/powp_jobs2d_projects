97725c4c87fcfe0ef10540396324fd5149f666a7
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2025-01-25 18:10:44 +0100

    Add visitor functions to driver composite

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.visitor.IVisitor;
 
 public class DriverComposite implements Job2dDriver {
     private List<Job2dDriver> drivers = new ArrayList<>();
@@ -23,6 +24,16 @@ public class DriverComposite implements Job2dDriver {
     public void setPosition(int x, int y) {
         for (Job2dDriver driver : drivers) {
             driver.setPosition(x, y);
+        }
+    }
+
+    public void accept(IVisitor visitor) {
+        visitor.visit(this);
+    }
+
+    public void visitDrivers(IVisitor visitor) {
+        for (Job2dDriver driver : drivers) {
+            visitor.visit(driver);
         }
     }
 }
