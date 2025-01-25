a84246c8033817bcfe7baa609c3d1b22f3920396
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2025-01-25 18:26:15 +0100

    Add overload for driver decorator

--- src/main/java/edu/kis/powp/jobs2d/drivers/visitor/DriversHierarchyVisitor.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/visitor/DriversHierarchyVisitor.java
@@ -2,6 +2,7 @@ package edu.kis.powp.jobs2d.drivers.visitor;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverComposite;
+import edu.kis.powp.jobs2d.drivers.adapter.DriverDecorator;
 
 public class DriversHierarchyVisitor implements IVisitor {
     @Override
@@ -10,6 +11,12 @@ public class DriversHierarchyVisitor implements IVisitor {
         driver.visitDrivers(this);
         // Code after
     }
+
+    @Override
+    public void visit(DriverDecorator driver) {
+        visit(driver.getDriver());
+    }
+
 
     @Override
     public void visit(Job2dDriver driver) {
--- src/main/java/edu/kis/powp/jobs2d/drivers/visitor/IVisitor.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/visitor/IVisitor.java
@@ -2,9 +2,10 @@ package edu.kis.powp.jobs2d.drivers.visitor;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverComposite;
+import edu.kis.powp.jobs2d.drivers.adapter.DriverDecorator;
 
 public interface IVisitor {
     void visit(DriverComposite driver);
-    //void visitObserver(driver);
     void visit(Job2dDriver driver);
+    void visit(DriverDecorator driver);
 }
