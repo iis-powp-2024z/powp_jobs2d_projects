37722b892f91074df41fbd6b865886757ca46f97
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2025-01-25 18:58:40 +0100

    Add line driver interface

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/LineDriver.java
@@ -0,0 +1,8 @@
+package edu.kis.powp.jobs2d.drivers;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.visitor.IVisitor;
+
+public interface LineDriver extends Job2dDriver {
+    void visit(IVisitor visitor);
+}
--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDriverAdapter.java
@@ -2,13 +2,13 @@ package edu.kis.powp.jobs2d.drivers.adapter;
 
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
-import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.drivers.LineDriver;
 import edu.kis.powp.jobs2d.drivers.visitor.IVisitor;
 
 /**
  * Line adapter - Job2dDriver with DrawPanelController object.
  */
-public class LineDriverAdapter implements Job2dDriver {
+public class LineDriverAdapter implements LineDriver {
     private ILine line;
     private int startX = 0, startY = 0;
     private String name;
@@ -42,6 +42,7 @@ public class LineDriverAdapter implements Job2dDriver {
         return "2d device simulator - " + name;
     }
 
+    @Override
     public void visit(IVisitor visitor) {
         visitor.visit(this);
     }
