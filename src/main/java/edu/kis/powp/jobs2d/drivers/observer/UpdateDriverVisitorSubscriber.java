64049ca13824836c3182730259b1b2116e2d736e
rukaSUKII <sobonukasz@gmail.com>
2025-01-27 22:26:44 +0100

    Amend update method for Visitor

--- src/main/java/edu/kis/powp/jobs2d/drivers/observer/UpdateDriverVisitorSubscriber.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/observer/UpdateDriverVisitorSubscriber.java
@@ -4,6 +4,7 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverComposite;
 import edu.kis.powp.jobs2d.drivers.LineSimulatorDriver;
 import edu.kis.powp.jobs2d.drivers.adapter.DriverDecorator;
+import edu.kis.powp.jobs2d.drivers.logger.LoggerDriver;
 import edu.kis.powp.jobs2d.drivers.visitor.DriversCountVisitor;
 import edu.kis.powp.jobs2d.features.DriverFeature;
 import edu.kis.powp.observer.Subscriber;
@@ -20,13 +21,18 @@ public class UpdateDriverVisitorSubscriber implements Subscriber {
 
     @Override
     public void update() {
-        Job2dDriver currentDriver = DriverFeature.getDriverManager().getCurrentDriver();
+        DriverDecorator driverDecorator = (DriverDecorator) DriverFeature.getDriverManager().getCurrentDriver();
+        Job2dDriver currentDriver = driverDecorator.getDriver();
+        if (currentDriver instanceof DriverDecorator) {
+            currentDriver = ((DriverDecorator) currentDriver).getDriver();
+        }
+
         if (currentDriver instanceof DriverComposite) {
             ((DriverComposite) currentDriver).accept(visitor);
-        } else if (currentDriver instanceof DriverDecorator) {
-            ((DriverDecorator) currentDriver).accept(visitor);
-        } else {
+        } else if (currentDriver instanceof LineSimulatorDriver) {
             ((LineSimulatorDriver) currentDriver).accept(visitor);
+        } else {
+            ((LoggerDriver) currentDriver).accept(visitor);
         }
         logger.info("Used drivers count: " + visitor.getDriversCount());
     }
