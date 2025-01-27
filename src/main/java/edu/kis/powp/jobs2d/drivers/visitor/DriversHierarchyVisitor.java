643b8366be661f0f87d7f1ab9037c303efc09e9d
rukaSUKII <sobonukasz@gmail.com>
2025-01-27 20:19:29 +0100

    Add method visit for handling LineSimulatorDriver

--- src/main/java/edu/kis/powp/jobs2d/drivers/visitor/DriversHierarchyVisitor.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/visitor/DriversHierarchyVisitor.java
@@ -2,6 +2,7 @@ package edu.kis.powp.jobs2d.drivers.visitor;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverComposite;
+import edu.kis.powp.jobs2d.drivers.LineSimulatorDriver;
 import edu.kis.powp.jobs2d.drivers.adapter.DriverDecorator;
 
 public class DriversHierarchyVisitor implements IVisitor {
@@ -16,5 +17,9 @@ public class DriversHierarchyVisitor implements IVisitor {
 
     @Override
     public void visit(Job2dDriver driver) {
+    }
+
+    @Override
+    public void visit(LineSimulatorDriver driver) {
     }
 }
