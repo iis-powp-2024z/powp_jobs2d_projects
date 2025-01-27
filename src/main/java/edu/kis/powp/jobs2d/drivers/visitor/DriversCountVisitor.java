ef568c2bfa9733b2c428b55f03ee1f98132cc472
rukaSUKII <sobonukasz@gmail.com>
2025-01-27 20:46:04 +0100

    Rename class implementing IVisitor

--- src/main/java/edu/kis/powp/jobs2d/drivers/visitor/DriversCountVisitor.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/visitor/DriversCountVisitor.java
@@ -5,13 +5,19 @@ import edu.kis.powp.jobs2d.drivers.DriverComposite;
 import edu.kis.powp.jobs2d.drivers.LineSimulatorDriver;
 import edu.kis.powp.jobs2d.drivers.adapter.DriverDecorator;
 
+import java.util.logging.Logger;
+
 public class DriversHierarchyVisitor implements IVisitor {
+
     @Override
     public void visit(DriverComposite driver) {
+        Logger logger = Logger.getLogger("global");
+        logger.info("Composite contains " + driver.countDrivers() + " drivers");
     }
 
     @Override
     public void visit(DriverDecorator driver) {
+
     }
 
 
