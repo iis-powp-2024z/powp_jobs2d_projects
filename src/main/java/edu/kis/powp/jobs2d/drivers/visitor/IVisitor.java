cca922c088a8a9e32293cb3813ed9c78b9e64e28
rukaSUKII <sobonukasz@gmail.com>
2025-01-27 22:25:16 +0100

    IVisitor interface update with LoggerDriver

--- src/main/java/edu/kis/powp/jobs2d/drivers/visitor/IVisitor.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/visitor/IVisitor.java
@@ -4,10 +4,12 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverComposite;
 import edu.kis.powp.jobs2d.drivers.LineSimulatorDriver;
 import edu.kis.powp.jobs2d.drivers.adapter.DriverDecorator;
+import edu.kis.powp.jobs2d.drivers.logger.LoggerDriver;
 
 public interface IVisitor {
     void visit(DriverComposite driver);
     void visit(Job2dDriver driver);
     void visit(DriverDecorator driver);
     void visit(LineSimulatorDriver driver);
+    void visit(LoggerDriver driver);
 }
