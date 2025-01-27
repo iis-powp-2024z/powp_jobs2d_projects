b4ccfbcfd0f81a5f34677270ba338fa596d2befa
rukaSUKII <sobonukasz@gmail.com>
2025-01-27 20:17:00 +0100

    Add method visit for LineSimulatorDriver

--- src/main/java/edu/kis/powp/jobs2d/drivers/visitor/IVisitor.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/visitor/IVisitor.java
@@ -2,10 +2,12 @@ package edu.kis.powp.jobs2d.drivers.visitor;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverComposite;
+import edu.kis.powp.jobs2d.drivers.LineSimulatorDriver;
 import edu.kis.powp.jobs2d.drivers.adapter.DriverDecorator;
 
 public interface IVisitor {
     void visit(DriverComposite driver);
     void visit(Job2dDriver driver);
     void visit(DriverDecorator driver);
+    void visit(LineSimulatorDriver driver);
 }
