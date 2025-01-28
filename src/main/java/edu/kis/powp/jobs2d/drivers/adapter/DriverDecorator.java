4ccbde8a8775729ab3181b6ff7eadbc24264acb0
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2025-01-28 09:58:46 +0100

    Override default implementations

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverDecorator.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverDecorator.java
@@ -2,9 +2,15 @@ package edu.kis.powp.jobs2d.drivers.adapter;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.VisitableJob2dDriver;
+import edu.kis.powp.jobs2d.drivers.visitor.IDriverVisitor;
 
 public interface DriverDecorator extends VisitableJob2dDriver {
     Job2dDriver getDriver();
 
     void setDriver(Job2dDriver driver);
+
+    @Override
+    default void accept(IDriverVisitor visitor) {
+        visitor.visit(this);
+    }
 }
--- src/main/java/edu/kis/powp/jobs2d/drivers/logger/LoggerDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/logger/LoggerDriver.java
@@ -1,7 +1,13 @@
 package edu.kis.powp.jobs2d.drivers.logger;
 
 import edu.kis.powp.jobs2d.drivers.VisitableJob2dDriver;
+import edu.kis.powp.jobs2d.drivers.visitor.IDriverVisitor;
 
 public interface LoggerDriver extends VisitableJob2dDriver {
     void setExtendedLogger(boolean extendedLogger);
+
+    @Override
+    default void accept(IDriverVisitor visitor) {
+        visitor.visit(this);
+    }
 }
