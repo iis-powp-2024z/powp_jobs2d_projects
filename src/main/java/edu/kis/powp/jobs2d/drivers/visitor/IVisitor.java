bca0f7366f72ad6e9bf56cd2467bdaf4c284dd33
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2025-01-28 09:19:57 +0100

    Use new driver instead of job2ddriver

--- src/main/java/edu/kis/powp/jobs2d/drivers/visitor/IVisitor.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/visitor/IVisitor.java
@@ -1,14 +1,14 @@
 package edu.kis.powp.jobs2d.drivers.visitor;
 
-import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverComposite;
 import edu.kis.powp.jobs2d.drivers.LineSimulatorDriver;
+import edu.kis.powp.jobs2d.drivers.VisitableJob2dDriver;
 import edu.kis.powp.jobs2d.drivers.adapter.DriverDecorator;
 import edu.kis.powp.jobs2d.drivers.logger.LoggerDriver;
 
 public interface IVisitor {
     void visit(DriverComposite driver);
-    void visit(Job2dDriver driver);
+    void visit(VisitableJob2dDriver driver);
     void visit(DriverDecorator driver);
     void visit(LineSimulatorDriver driver);
     void visit(LoggerDriver driver);
