1a45ecd67b0625768e497f49874c51394253c313
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2025-01-28 10:26:59 +0100

    Amend driver composite

--- src/main/java/edu/kis/powp/jobs2d/drivers/composite/IDriverComposite.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/composite/IDriverComposite.java
@@ -2,7 +2,16 @@ package edu.kis.powp.jobs2d.drivers.composite;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.VisitableJob2dDriver;
+import edu.kis.powp.jobs2d.drivers.visitor.IDriverVisitor;
+
+import java.util.List;
 
 public interface IDriverComposite extends VisitableJob2dDriver {
     void addDriver(Job2dDriver driver);
+    List<Job2dDriver> getDrivers();
+
+    @Override
+    default void accept(IDriverVisitor visitor) {
+        visitor.visit(this);
+    }
 }
