9c71879aff51f7147d48a73c57c7067d04bab686
Grzegorz Kubicki <grzechu.kubicki@o2.pl>
2025-01-28 17:30:41 +0100

    Change to visitableJob2dDriver

--- src/main/java/edu/kis/powp/jobs2d/drivers/SelectDriverMenuOptionListener.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/SelectDriverMenuOptionListener.java
@@ -1,15 +1,13 @@
 package edu.kis.powp.jobs2d.drivers;
 
-import edu.kis.powp.jobs2d.Job2dDriver;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SelectDriverMenuOptionListener implements ActionListener {
     private final DriverManager driverManager;
-    private Job2dDriver driver = null;
+    private VisitableJob2dDriver driver = null;
 
-    public SelectDriverMenuOptionListener(Job2dDriver driver, DriverManager driverManager) {
+    public SelectDriverMenuOptionListener(VisitableJob2dDriver driver, DriverManager driverManager) {
         this.driverManager = driverManager;
         this.driver = driver;
     }
